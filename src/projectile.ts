f5d04a8de0290d9457021e73a6d0ed3f2644c59a
Elijah Polyakov <Elijah Polyakov>
2017-02-26 21:36:23 -0500

    tank moves

--- src/projectile.ts
+++ src/projectile.ts
@@ -2,8 +2,12 @@
 
 class Projectile {
     position: Point;
-    
+    velX: number;
+    velY: number;
     constructor (x: number, y: number, velX: number, velY: number) {
         this.position = new Point(x,y);
+
+        this.velX = 10;
+        this.velY = 10;
     }
 }
--- src/tank.ts
+++ src/tank.ts
@@ -5,12 +5,18 @@
 class Tank {
     name: string = "hello world!";   
     position: Point = new Point(0,0);
-    
+    moveS: number = 0;
+    moveA: number = 0;
     move(deltaX: number, deltaY: number): void {
-        this.position.x += deltaX;
-        this.position.y += deltaY;
+        //this.position.x += deltaX;
+        //this.position.y += deltaY;
+        this.moveS = 300;
+
+
     }
-    
+
+
+
     shoot(): Projectile {
         console.log("Creating project at position - x: " + this.position.x + ", y: "+ this.position.y );
         let directionX: number = 100;
